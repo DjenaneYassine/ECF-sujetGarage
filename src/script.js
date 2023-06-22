e227ec9a4d9f6f7ef84adb83209183b2691620e5
djenane <ydjenane@calasys.fr>
2023-06-22 11:29:59 +0200

    Layout page principale fini

---
+++ src/script.js
@@ -0,0 +1,7 @@
+let modal = $('.modal');
+let btnModalDevis = document.querySelector("#btnModalDevis")
+
+btnModalDevis.on("click", function(){
+    modal.css({"visibility":"visible", "opacity": "1"})
+})
+
